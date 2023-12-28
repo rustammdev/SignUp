d2d500768756a6cfb90449bd02cc414f81d85100
Rustamjon Nuriddinov <rustamcheek11@gmail.com>
2023-12-28 12:23:42 +0500

    Foydalanuvchi malumoti topilmasa chiqishi kerak bo'ladigan ogohlantirish malumotlari qo'shildi

--- form.js
+++ form.js
@@ -1,9 +1,11 @@
 const form = document.querySelector('#form')
+const btn = document.querySelector('#btn')
 
 const passJson = {
     login : '',
     password : ''
 }
+
 // Bosh listga tekshiruv
 const clc = document.querySelectorAll(".clc")
 
@@ -16,12 +18,39 @@ form.addEventListener('submit', (e) => {
         clc.forEach((e) => {
             e.classList.add('required')
         })
+    }else {
+        /* ---------- create akkount uchun ---------- */
+        /* 
+        let usernameRegex = /^[a-zA-Z0-9]{6, 9999}+$/;
+        let passwordRegex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+
+        if (!(usernameRegex.test(form.username.value))){
+            e.preventDefault()
+            clc[0].classList.add('required-login')
+        }
+
+        if (!(passwordRegex.test(form.password.value))){
+            e.preventDefault()
+            clc[1].classList.add('required-password')
+        } */
+
+        let err = false
+        if (!(err)) {
+            e.preventDefault()
+            clc.forEach((e) => {
+                e.classList.add('required2')
+            })
+        }
     }
+
+    
 })
 
+// Login yozish boshlanganda xatoni o'chirish 
 const inpUsr = document.querySelector('#username')
 inpUsr.addEventListener('keydown', () => {
     clc.forEach((e) => {
         e.classList.remove('required')
+        e.classList.remove('required-login')
     })
 })
